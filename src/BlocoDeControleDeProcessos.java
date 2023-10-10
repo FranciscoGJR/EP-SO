59d3c2222f76683c8ea5d8b3aebe3a47b6592fff
Daniel Roveri <1danroveri1@gmail.com>
2023-10-10 14:11:41 -0300

    Modificando BCP e TabelaDeProcessos

--- src/BlocoDeControleDeProcessos.java
+++ src/BlocoDeControleDeProcessos.java
@@ -1,6 +1,79 @@
 public class BlocoDeControleDeProcessos{
 
-    BlocoDeControleDeProcessos(){
-        
+    private int PC; //Program Counter
+    private int estadoDoProcesso;
+    private int x;
+    private int y;
+    private String[] programa;
+    private String nomeDoPrograma;
+    private int tamPrograma;
+
+    BlocoDeControleDeProcessos(int tamPrograma){ //Adicionar arquivo txt como parâmetro
+        this.tamPrograma = tamPrograma;
+        this.PC = 1;
+        this.x = 0;
+        this.y = 0;
+        this.programa = new String[tamPrograma];
+        //Popular programa com o arquivo txt
+        this.nomeDoPrograma = programa[0];
+    }
+
+    public int getTamPrograma() {
+        return tamPrograma;
+    }
+
+    public void setTamPrograma(int tamPrograma) {
+        this.tamPrograma = tamPrograma;
+    }
+
+    public int getPC() {
+            return PC;
+    }
+
+    public void setPC(int PC) {
+        this.PC = PC;
+    }
+
+    public int getEstadoDoProcesso() {
+        return estadoDoProcesso;
+    }
+
+    public void setEstadoDoProcesso(int estadoDoProcesso) {
+        if(estadoDoProcesso >= 1 && estadoDoProcesso <= 3 ){
+            this.estadoDoProcesso = estadoDoProcesso;
+        }
+        else System.out.println("Estado inválido!");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String[] getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String[] programa) {
+        this.programa = programa;
+    }
+    
+    public String getNomeDoPrograma() {
+        return nomeDoPrograma;
+    }
+
+    public void setNomeDoPrograma(String nomeDoPrograma) {
+        this.nomeDoPrograma = nomeDoPrograma;
     }
 }
--- src/TabelaDeProcessos.java
+++ src/TabelaDeProcessos.java
@@ -1,6 +1,26 @@
 public class TabelaDeProcessos{
 
-    TabelaDeProcessos(){
-        
+    private BlocoDeControleDeProcessos[] tabela;
+    private int numProcessos;
+
+    TabelaDeProcessos(int numProcessos){
+        this.numProcessos = numProcessos;
+        this.tabela = new BlocoDeControleDeProcessos[numProcessos];
+    }
+
+    public int getNumProcessos() {
+        return numProcessos;
+    }
+
+    public void setNumProcessos(int numProcessos) {
+        this.numProcessos = numProcessos;
+    }
+
+    public BlocoDeControleDeProcessos[] getTabela() {
+        return tabela;
+    }
+
+    public void setTabela(BlocoDeControleDeProcessos[] tabela) {
+        this.tabela = tabela;
     }
 }
