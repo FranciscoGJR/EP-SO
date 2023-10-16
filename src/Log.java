7626e61a5cb064560ff3978aac25f4ff7494264d
Daniel Roveri <1danroveri1@gmail.com>
2023-10-16 17:43:50 -0300

    Implementando Log.java

--- src/Log.java
+++ src/Log.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
@@ -7,47 +9,91 @@ public class Log {
     private Integer trocas;
     private Integer instrucoes;
     private Integer es;
-    private Path diretorio;
-
-    Log() throws IOException{
+    private Path arquivo;
+    Log(){
+        es = 0;
         this.trocas = -1;
         instrucoes = 0;
-        //diretorio = Path do log.txt
-        if(Files.notExists(diretorio)){
-            Files.createFile(diretorio);
+        String diretorioAtual = System.getProperty("user.dir");
+        arquivo = Paths.get(diretorioAtual, "log.txt");
+        try{
+            if(Files.notExists(arquivo)){
+                Files.createFile(arquivo);
+            }
+            else {
+                Files.writeString(arquivo, "");
+            }
+            
+        } catch (IOException e){
+            e.printStackTrace();
+            throw new RuntimeException("Erro no log.txt");
         }
-        else {
-            //Apagar texto contido
-        }
+
+
     }
 
     public void executando(String nomePrograma){
+        
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            linhas.add("Executando " + nomePrograma);           
+            Files.write(arquivo, linhas);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }
+
+
         //Escrever Executando TESTE-1
     }
 
     public void interrompendo(String nomePrograma, Integer numInstrucoes){
         trocas++;
         instrucoes = instrucoes+numInstrucoes;
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            linhas.add("Interrompendo " + nomePrograma + " após " + numInstrucoes + " instruções");           
+            Files.write(arquivo, linhas);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }
         //Escrever Interrompendo Teste-1 após n instruções
-
     }
 
-    public void es(String nomePrograma){
+    public void entradaSaida(String nomePrograma){
         es++;
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            linhas.add("E/S iniciada em " + nomePrograma);           
+            Files.write(arquivo, linhas);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }
         //E/S iniciada em TESTE-9
     }
 
-    public void terminandoProcesso(String nomePrograma, Integer X, Integer Y){
-        //TESTE-1 terminado. X=3. Y=1
+    public void terminandoProcesso(String nomePrograma, Integer x, Integer y){
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            linhas.add(nomePrograma + " terminado " + "X=" + x + ". Y=" +y);           
+            Files.write(arquivo, linhas);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void terminandoEscalonador(Integer quantum){
         Integer mediaInstrucoes = instrucoes/trocas;
         Integer mediaTrocas = trocas/quantum;
 
-        //MEDIA DE TROCAS: 5
-        //MEDIA DE INSTRUCOES: 2.5   
-        //QUANTUM: 3
+        try{
+            List<String> linhas = Files.readAllLines(arquivo);
+            linhas.add("MEDIA DE TROCAS: " + mediaTrocas);
+            linhas.add("MEDIA DE INSTRUCOES: " + mediaInstrucoes);
+            linhas.add("Quantum: " + quantum);
+            Files.write(arquivo, linhas);
+        }   catch(IOException e){
+            e.printStackTrace();
+        }
 
     }
 
